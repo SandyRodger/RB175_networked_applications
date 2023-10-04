e4fe9a73f218845c9416b9524f82ee42b6cf525a
Sandy Rodger <abmrodger@gmail.com>
2023-10-04 10:02:40 +0100

    heroku

--- 03_working_with_sinatra/04_code_challenge_dynamic_directory_index/directory.rb
+++ 03_working_with_sinatra/04_code_challenge_dynamic_directory_index/directory.rb
@@ -25,3 +25,12 @@ contents.each do |filename|
     @file= File.read("data/#{filename}")
   end
 end
+
+get '/miles.jpg' do
+  erb :image_view
+end
+
+get '/images' do
+  @all_images = Dir.entries('/Users/sandyboy/Desktop/RB175_networked_applications/03_working_with_sinatra/04_code_challenge_dynamic_directory_index/public/images').sort
+  erb :image_view
+end
